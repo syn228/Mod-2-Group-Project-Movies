96ec3713000d962eceeed98ae1b95090247fa7d1
cupofjoy <eelantung@gmail.com>
2018-05-29 17:35:12 -0400

    push

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,24 +4,20 @@ class Movie < ApplicationRecord
   has_many :casts
   has_many :actors, through: :casts
 
-  def search_movies(input)
+  def self.search_movies(input)
     movie_results = RestClient.get("https://api.themoviedb.org/3/search/movie?api_key=945dbc0976d1fcb4db817690ca47fd3b&language=en-US&query=#{input}&page=1&include_adult=false")
 
     parsed_movies = JSON.parse(movie_results)
-    parsed_movies["results"].each_with_index { |movies, i|
-      "#{i+1}. #{movies["title"]}"
-    }
+    movie_result = parsed_movies["results"]
 
-    puts "Please pick movie title by number:"
-    input = gets.chomp
-
-    get_movie_info(input)
   end
 
   def get_movie_info(input)
     movie_id = parsed_movies[(input-1)]["id"]
     movie = RestClient.get("https://api.themoviedb.org/3/movie/#{movie_id}?api_key=945dbc0976d1fcb4db817690ca47fd3b&language=en-US")
     parsed_movie = JSON.parse(movie)
+
+
 
     movie_title = parsed_movie["title"]
     movie_genre = parsed_movie["genres"].map { |genre| genre["name"] }.join(", ")
@@ -39,6 +35,8 @@ class Movie < ApplicationRecord
       character_array << c["character"]
       actor_array << c["name"]
     }
+
+    {|c, i| "#{c}: #{actor_arra}"}
   end
 
 
