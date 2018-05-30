5986056a892de5cd406b7bbae3f29b84c15b09ae
Aaron Na <syn228@nyu.edu>
2018-05-30 10:47:58 -0400

    addded actor bio, need to figure out actor id issue in movies/show

--- app/controllers/generals_controller.rb
+++ app/controllers/generals_controller.rb
@@ -9,6 +9,12 @@ class GeneralsController < ApplicationController
         @movies.each do |movie|
       Movie.get_movie_info(movie["id"])
       end
+      # @movies.each_with_index do |m, i|
+      #   if i > 15
+      #     sub_array = []
+      #     sub_array << @movies[i..-1]
+      #   end
+      # end
     else
       @movies = Movie.all.order("created_at DESC")
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -27,28 +27,40 @@ class Movie < ApplicationRecord
     Movie.find_or_create_by(name: movie_title, genre: movie_genre, budget: movie_budget, producer: movie_production, api_movie_id: input)
   end
 
-  def get_character(input)
+  def get_character(input, movie_name)
     cast = RestClient.get("https://api.themoviedb.org/3/movie/#{input}/credits?api_key=945dbc0976d1fcb4db817690ca47fd3b")
     parsed_cast = JSON.parse(cast)
     character_array = []
     parsed_cast["cast"].each {|c|
       character_array << c["character"]
-      Character.create(name: c["character"])
+      @character = Character.find_or_create_by(name: c["character"])
     }
+    @character.movies << movie_name
     roles = character_array.each {|c| c }
     return roles
+
   end
 
-  def get_actor(input)
+  def get_actor(input, movie_name)
     cast = RestClient.get("https://api.themoviedb.org/3/movie/#{input}/credits?api_key=945dbc0976d1fcb4db817690ca47fd3b")
     parsed_cast = JSON.parse(cast)
     actor_array = []
-    parsed_cast["cast"].each {|c|
+    parsed_cast["cast"].each_with_index {|c, i|
+      break if i == 15
       actor_array << c["name"]
-      Actor.create(name: c["name"])
+      @actor = Actor.find_or_create_by(name: c["name"])
+      Movie.get_actor_bio(c["id"])
     }
+    @actor.movies << movie_name
     roles = actor_array.each {|c| c }
     return roles
   end
 
+  def self.get_actor_bio(input)
+    ac = RestClient.get("https://api.themoviedb.org/3/person/#{input}?api_key=945dbc0976d1fcb4db817690ca47fd3b&language=en-US")
+    parsed_actor = JSON.parse(ac)
+    a_bio = parsed_actor["biography"]
+    a = Actor.find_by(name: parsed_actor["name"])
+    a.update(bio: a_bio)
+  end
 end
