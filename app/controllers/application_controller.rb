31eb16fee7ce839d88b7676bc7234d647ef0f75f
Aaron Na <syn228@nyu.edu>
2018-05-31 15:05:29 -0400

    Made some style changes, starting specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
   def current_user
     if session[:user_id]
       User.find(session[:user_id])
-    end  
+    end
   end
 
   def logged_in?
