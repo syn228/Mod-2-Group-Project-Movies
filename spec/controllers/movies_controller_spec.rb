fe516e917fa94108205f42534c93eeb17baa8720
Aaron Na <syn228@nyu.edu>
2018-05-31 16:33:04 -0400

    Putting spec on hold, working snapshot

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,24 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController do
-  let(:attributes) do
-  {
-    name:"The Avengers",
+#   it "returns http success" do
+#     movie = Movie.create(name:"The Avengers", genre:"Science Fiction, Action, Adventure", budget:"220000000", producer:"Marvel Studios")
+#     get :show, id: movie.id
+#     expect(response).to have_http_status(:success)
+#   end
+# end
+  # let(:attributes) do
+  # {
+  #   name:"The Avengers",
+  #   genre:"Science Fiction, Action, Adventure",
+  #   budget:"220000000",
+  #   producer:"Marvel Studios",
+  # }
+
+  # end
+  let(:article) {Movie.create!(name:"The Avengers",
     genre:"Science Fiction, Action, Adventure",
     budget:"220000000",
-    producer:"Marvel Studios",
-  }
-  end
-let(:article_found) { Movie.find(@article.id)}
-
-  before do
-    @article = Movie.create!(attributes)
-  end
+    producer:"Marvel Studios")}
+  let(:article_found) { Movie.find(id: article.id)}
 
   describe "showing a movie" do
     it "shows a movie" do
-      get :show, id: @article.id
-      expect(article_found).to eq(@article)
+
+      get :show, id: article.id
+      expect(article_found.id).to eq(article.id)
     end
   end
 end
